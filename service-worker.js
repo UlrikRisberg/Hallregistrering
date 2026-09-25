fc90d304ee63e6a5cd72e76ca8edafc4b75e1733
UlrikRisberg <ulrik@vareggmedia.no>
2026-09-25 21:53:08 +0200

    Update cache name to hallreg-cache-v2

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js");
 
-const CACHE_NAME = "hallreg-cache-v1";
+const CACHE_NAME = "hallreg-cache-v2";
 const APP_SHELL = [
   "./",
   "./index.html",
